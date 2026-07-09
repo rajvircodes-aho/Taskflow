f832a5e9f0d34e6304f22c4b1b2ab36356d9b4ba
rajvircodes-aho <rajvircodes@gmail.com>
2026-07-09 13:21:48 +0530

    user model created

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
+const connectToDB = require("./src/config/database")
+
+connectToDB()
 
 app.listen(3000,() => {
 console.log("server is running")
