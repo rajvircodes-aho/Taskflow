7359d7ab581764a8ea56ce037fa8c5c54e874cd4
rajvircodes-aho <rajvircodes@gmail.com>
2026-07-13 12:23:41 +0530

    logout function added

--- frontend/src/app/homepage/page.js
+++ frontend/src/app/homepage/page.js
@@ -1,9 +1,14 @@
+"use client";
 import React from 'react'
+import { useAuth } from "../hooks/useAuth";
 
 const page = () => {
+
+  const { handleLogout } = useAuth()
   return (
     <div>
       homepage
+     <button className ="text-white bg-amber-950" onClick={handleLogout}>logout</button>
     </div>
   )
 }
